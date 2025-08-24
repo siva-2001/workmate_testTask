a52480fa01c430827a16ee1f75ba0e4a0c4acc5f
siva-2001 <slava.kutolvas@gmail.com>
2025-08-24 16:37:32 +0700

    add README

--- conftest.py
+++ conftest.py
@@ -4,3 +4,4 @@ from router import router
 @pytest.fixture(scope='module')
 def router_object():
     yield router
+
--- main.py
+++ main.py
@@ -14,5 +14,4 @@ if __name__ == "__main__":
     args = getArgs().__dict__
     reportType = args.pop("report")
     handler = router.get_method(reportType)
-    if(handler):
-        handler(**args)
+    if(handler): handler(**args)
